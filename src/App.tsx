07b9ce68fa2705132e8d27656b58fe2de12e9db1
Daniel <rdjmartinez95@gmail.com>
2022-12-15 17:27:03 -0300

    Actualizaciones

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
-import './App.css';
-import InputField from './components/InputField';
+import React, { useState } from "react";
+import "./App.css";
+import InputField from "./components/InputField";
+import TodoList from "./components/TodoList";
+import { Todo } from "./model";
 
 // React.FC = functional component
 const App: React.FC = () => {
-  return (<div className="App">
-  <span className="heading">Taskify</span>
-  <InputField/>
-  </div> 
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([])
+  console.log(todo);
+  
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, {id: Date.now(), todo, isDone:false }])
+      
+    }
+    setTodo("")
+  }
+console.log(todos);
+
+  return (
+    <div className="App">
+      <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      <TodoList todos={todos} setTodos={setTodos}/>
+    </div>
   );
 };
 // hello
 export default App;
-  
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import './styles.css'
+import React, {useRef} from "react";
+import "./styles.css";
 
-const InputField = () => {
-  return (
-    <form className="input">
-      <input type="input" placeholder='Enter a task' className='input__box'/>
-      <button className='input__submit' type='submit'>Go</button>
-
-    </form>
-  )
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
+
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null)  
+  return (
+    <form className="input" 
+    onSubmit={(e)=> {
+      handleAdd(e);
+      inputRef.current?.blur();
+    }
+  }
+  >
+      <input
+        type="input"
+        ref={inputRef}
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        placeholder="Enter a task"
+        className="input__box"
+      />
+      <button className="input__submit" type="submit">
+        Go
+      </button>
+    </form>
+  );
+};
 // hello
-export default InputField
+export default InputField;
