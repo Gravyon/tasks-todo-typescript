f08a33e6bd758ad5f5463c904218c20c85f9a703
Daniel <rdjmartinez95@gmail.com>
2022-12-22 09:24:49 -0300

    Drag 'n drop feature

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,38 @@ import "./App.css";
 import InputField from "./components/InputField";
 import TodoList from "./components/TodoList";
 import { Todo } from "./model";
+import { DragDropContext } from "react-beautiful-dnd";
 
 // React.FC = functional component
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
-  const [todos, setTodos] = useState<Todo[]>([])
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [completedTodos, setCompletedTodos] = useState<Todo[]>([]);
   console.log(todo);
-  
+
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
     if (todo) {
-      setTodos([...todos, {id: Date.now(), todo, isDone:false }])
-      
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
     }
-    setTodo("")
-  }
-console.log(todos);
+    setTodo("");
+  };
+  console.log(todos);
 
   return (
-    <div className="App">
-      <span className="heading">Taskify</span>
-      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
-      <TodoList todos={todos} setTodos={setTodos}/>
-    </div>
+    <DragDropContext onDragEnd={() => {}}>
+      <div className="App">
+        <span className="heading">Taskify</span>
+        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+        <TodoList
+          todos={todos}
+          setTodos={setTodos}
+          completedTodos={completedTodos}
+          setCompletedTodos={setCompletedTodos}
+        />
+      </div>
+    </DragDropContext>
   );
 };
-// hello
+
 export default App;
